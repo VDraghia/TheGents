0cb3be32f08da4bff9abb8d80d25335bb651dcd1
Nooks <61255068+Nooks@users.noreply.github.com>
2020-11-17 14:04:02 -0500

    Add files via upload

---
+++ LoginModel.cs
@@ -0,0 +1,21 @@
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectManagementCollection.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+
+        [Required]
+        [PasswordPropertyText]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+
+        
+    }
+}
